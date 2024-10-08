d8bb6609b4bb1f4900f5aa22b6be0cf0169e6392
Jay Prakash <jayp7808@gmail.com>
2024-10-08 19:59:43 +0530

    Added Google Analytics

--- digital-boost-landing-page/src/pages/landingPage/LandingPage.jsx
+++ digital-boost-landing-page/src/pages/landingPage/LandingPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import './landingPage.css';
 import AOS from 'aos'; // Import AOS
 import 'aos/dist/aos.css'; // Import AOS styles
-
+import ReactGA from 'react-ga';
 import Hero from '../../components/hero/Hero';
 import Navbar from '../../components/navbar/Navbar';
 import Features from '../../components/features/Features';
@@ -13,12 +13,21 @@ import BlogPosts from '../../components/blogPosts/BlogPosts';
 import FaqSection from '../../components/faqSection/FaqSection';
 
 const LandingPage = () => {
+  const GA_MEASUREMENT_ID = 'G-9NPPTLX31F';
+
   // Initialize AOS when the component mounts
   useEffect(() => {
     AOS.init({
       duration: 2500, // Animation duration in milliseconds
       once: true, // Animation happens only once
     });
+
+    // Initialize Google Analytics
+    ReactGA.initialize(GA_MEASUREMENT_ID);
+
+    // Track the initial pageview
+    ReactGA.pageview(window.location.pathname + window.location.search);
+
   }, []);
 
   return (
